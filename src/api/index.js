54e79ff00e5eda08297b578bdb979ca28c859008
dustleafwjl <wangjianglin_priv@qq.com>
2020-09-15 14:02:37 +0800

    [jianglin] feat: add api

---
+++ src/api/index.js
@@ -0,0 +1,26 @@
+const baseUrl = "http://localhost:8080";
+
+export const getUserInfo = async (userId) => {
+  const fetchData = await fetch(`${baseUrl}/users/${userId}`);
+  const result = await fetchData.json();
+  return result;
+};
+
+export const getEducationsByUserId = async (userId) => {
+  const fetchData = await fetch(`${baseUrl}/users/${userId}/educations`);
+  const result = await fetchData.json();
+  return result;
+};
+
+export const createUser = async (user) => {
+  const fetchData = await fetch(`${baseUrl}/users`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      body: JSON.stringify(user),
+    },
+  });
+  const result = await fetchData.json();
+  return result;
+};
---
+++ src/render/renderHeader.js
@@ -0,0 +1,10 @@
+import $ from "jquery";
+
+const renderHeader = () => {
+  $(".header-avatar").attr(
+    "src",
+    "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1600151586026&di=b45aa215fde87578c6cfafc3f12b391c&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201706%2F10%2F20170610192627_yhAMN.thumb.700_0.jpeg"
+  );
+};
+
+export default renderHeader;
