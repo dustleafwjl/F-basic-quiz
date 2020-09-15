cc7542d7a40de3b25e6bc06d25613fc91091dd0d
dustleafwjl <wangjianglin_priv@qq.com>
2020-09-15 14:26:29 +0800

    [jianglin] feat: add init and renderEducation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import "./style/index.scss";
-import { getUserInfo } from "./api";
+import { getUserInfo, getEducationsByUserId } from "./api";
 import renderUserInfo from "./render/renderUserInfo";
+import renderEducation from "./render/renderEducation";
 
 function initUserInfo() {
   getUserInfo(1).then((user) => {
@@ -8,4 +9,11 @@ function initUserInfo() {
   });
 }
 
+function initEducation() {
+  getEducationsByUserId(1).then((educations) => {
+    renderEducation(educations);
+  });
+}
+
 initUserInfo();
+initEducation();
--- src/render/renderEducation.js
+++ src/render/renderEducation.js
@@ -1,1 +1,24 @@
-// import $ from "jquery";
+import $ from "jquery";
+
+const renderEducation = (educations) => {
+  let educationHtml = "";
+  educations.forEach((education) => {
+    const itemHtml = `<section class="education-element">
+      <div class="element-year">
+        ${education.year}
+      </div>
+      <div class="element-experience">
+        <p class="element-experience-event">
+          ${education.title}
+        </p>
+        <p class="element-experience-description">
+          ${education.description}
+        </p>
+      </div>
+    </section>`;
+    educationHtml += itemHtml;
+  });
+  $(".education-list").html(educationHtml);
+};
+
+export default renderEducation;
